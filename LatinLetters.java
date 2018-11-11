325c3414e71f3dc2f6e8fd61c926bd3497ffd6e0
Daniela Pavlova <43967035+danniesmile@users.noreply.github.com>
2018-11-11 21:18:20 +0000

    Add files via upload

---
+++ LatinLetters.java
@@ -0,0 +1,11 @@
+package loops;
+ 
+public class LatinLetters {
+    public static void main(String[] args) {
+ 
+ 
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println(i);
+        }
+    }
+}
